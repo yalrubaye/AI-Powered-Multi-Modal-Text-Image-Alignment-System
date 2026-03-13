0b38f3951dedab40494dcd76775233ab581f8b87
Yousif Alrubaye <yousif.alrubaye1994@gmail.com>
2026-03-12 23:57:12 -0700

    Update model.py

--- model.py
+++ model.py
@@ -11,6 +11,7 @@ class MultiModalModel(nn.Module):
             nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),  # Output: 32 x 14 x 14
+            nn.MaxPool2d(kernel_size=7, stride=3),  # Output: 32 x 14 x 15
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),  # Output: 64 x 7 x 7
@@ -33,4 +34,5 @@ class MultiModalModel(nn.Module):
         
 
         return image_embedding, text_embedding
+
 
